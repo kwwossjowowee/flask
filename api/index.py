2a500ab8f837bab581755ddee26b74586c59687c
kwwossjowowee <istegebagli830@gmail.com>
2024-11-02 17:59:23 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,17 +7,18 @@ api = f"https://api.telegram.org/bot{token}"
 @app.route('/', methods=["POST"])
 def home():
   data = request.get_json()
+  metin = None
   try:
     message = data["message"]
-    message["text"]
+    metin = message["text"] if message["text"] else message["caption"]
     chat = message["chat"]
     from_user = message["from"]
   except:
     return "False"
-  if message["text"] == "/start":
+  if metin == "/start": 
     requests.get(f"{api}/sendMessage", data={"chat_id": chat["id"], "text": "Welcome:)"})
   else:
-    translated = GoogleTranslator(source='auto', target=from_user["language_code"]).translate(message["text"])
+    translated = GoogleTranslator(source='auto', target=from_user["language_code"]).translate(metin)
     requests.get(f"{api}/sendChatAction", data={"chat_id": chat["id"], "action": "typing"})
     requests.get(f"{api}/sendMessage", data={"chat_id": chat["id"], "text": translated})
   return "True"
