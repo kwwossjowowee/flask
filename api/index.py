6b721124a920d20265018cfffedde429c5272bcc
kwwossjowowee <istegebagli830@gmail.com>
2025-01-07 16:49:03 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,24 +1,13 @@
 from flask import *
-import requests 
-from deep_translator import GoogleTranslator
+
 app = Flask(__name__)
-token = "7739744719:AAHQYcZCyMwO9rikqK8DSlz3FX_P6mIiUVQ"
-api = f"https://api.telegram.org/bot{token}"
-@app.route('/', methods=["POST"])
-def home():
-  data = request.get_json()
-  metin = None
-  try:
-    message = data["message"]
-    metin = message["text"] if message.get("text") else message["caption"]
-    chat = message["chat"]
-    from_user = message["from"]
-  except:
-    return "False"
-  if metin == "/start": 
-    requests.get(f"{api}/sendMessage", data={"chat_id": chat["id"], "text": "Welcome:)"})
-  else:
-    requests.get(f"{api}/sendChatAction", data={"chat_id": chat["id"], "action": "typing"})
-    translated = GoogleTranslator(source='auto', target=from_user["language_code"]).translate(metin)
-    requests.get(f"{api}/sendMessage", data={"chat_id": chat["id"], "text": translated})
-  return "True"
+
+@app.route('/')
+def index():
+    # X-Forwarded-For başlığını al
+    forwarded_for = request.headers.get('X-Forwarded-For')
+    if forwarded_for:
+        ip_address = forwarded_for.split(',')[0]  # İlk IP adresini al
+    else:
+        ip_address = request.remote_addr  # Doğrudan IP adresini al
+    return f"IP Address: {ip_address}"
